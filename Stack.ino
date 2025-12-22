38c5c4a95676cfe95385c5bffe858c350ecb4710
manututelea <manututelea@yahoo.com>
2025-12-22 23:52:38 +0200

    LCD nou cu i2c si meniu de alegere lvl

--- Stack.ino
+++ Stack.ino
@@ -1,46 +1,80 @@
-#include <LiquidCrystal.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
 
-const int BUTTON_PIN = 8;
+const int BTN_SCHIMBA = 8; 
+const int BTN_START = 9;
 
 int level = 1;
 const int totalNivele = 4;
-int lastButtonState = LOW;
 
-void select_level() {
-    int cursorPositions[] = {0, 3, 6, 10}; // pozitiile unde pune caracterul pentru a alege nivelul
-    int currentPosition = cursorPositions[level - 1]; // incepe cu cursorul de la lvl 1
+int lastStateSchimba = HIGH; 
+int lastStateStart = HIGH;
+
+void select_level() 
+{
+    int cursorPositions[] = {0, 3, 6, 10}; 
+    int currentPosition = cursorPositions[level - 1]; 
 
     lcd.setCursor(0, 0); 
     lcd.print("                "); 
-
     lcd.setCursor(0, 1);
     lcd.print("1  2  3  inf");
-    
     lcd.setCursor(currentPosition, 0); 
     lcd.print(">"); 
+}
+
+void porneste_jocul() 
+{
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("START JOC!");
     
+    lcd.setCursor(0, 1);
+    if (level == 4) 
+    {
+        lcd.print("Mod: Infinit");
+    } 
+    else 
+    {
+        lcd.print("Nivel: ");
+        lcd.print(level);
+    }
+    
+    delay(2000); 
+    select_level(); 
 }
 
-
-void setup() {
-    lcd.begin(16, 2);
-    pinMode(BUTTON_PIN, INPUT); 
-    select_level();
+void setup() 
+{
+    lcd.init();        
+    lcd.backlight();    
+    pinMode(BTN_SCHIMBA, INPUT_PULLUP); 
+    pinMode(BTN_START, INPUT_PULLUP);
+    select_level();     
 }
 
-void loop() {
-    int currentButtonState = digitalRead(BUTTON_PIN);
+void loop() 
+{
+    int stareSchimba = digitalRead(BTN_SCHIMBA);
+    int stareStart = digitalRead(BTN_START);
 
-    if (currentButtonState == HIGH && lastButtonState == LOW) {
+    if (stareSchimba == LOW && lastStateSchimba == HIGH) 
+    {
         level++;
-        if (level > totalNivele) {
+        if (level > totalNivele) 
+        {
             level = 1;
         }
-        select_level();
-        delay(100); 
+        select_level(); 
+        delay(200);
     }
-    lastButtonState = currentButtonState;
+    lastStateSchimba = stareSchimba;
+
+    if (stareStart == LOW && lastStateStart == HIGH) 
+    {
+        porneste_jocul();
+    }
+    lastStateStart = stareStart;
 }
