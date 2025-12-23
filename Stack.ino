2290a452002670d4dc589820f16cb2c036816b68
manututelea <manututelea@yahoo.com>
2025-12-24 01:43:29 +0200

    LCD terminat + buzzer + inceput matrice de led uri

--- Stack.ino
+++ Stack.ino
@@ -1,16 +1,22 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <LedControl.h> 
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); 
+LedControl lc = LedControl(12, 11, 10, 4); 
 
 const int BTN_SCHIMBA = 8; 
 const int BTN_START = 9;
+const int BUZZER_PIN = 7; 
 
 int level = 1;
 const int totalNivele = 4;
 
 int lastStateSchimba = HIGH; 
 int lastStateStart = HIGH;
+
+bool jocPornit = false; 
+int viteza = 100;
 
 void select_level() 
 {
@@ -25,56 +31,140 @@ void select_level()
     lcd.print(">"); 
 }
 
-void porneste_jocul() 
+void sunetPlasare() 
+{
+    digitalWrite(BUZZER_PIN, LOW); 
+    delay(50); 
+    digitalWrite(BUZZER_PIN, HIGH);
+}
+
+void sunetCastig() 
+{
+    for(int i=0; i<3; i++)
+    {
+        digitalWrite(BUZZER_PIN, LOW);  
+        delay(100); 
+        digitalWrite(BUZZER_PIN, HIGH); 
+        delay(100); 
+    }
+    digitalWrite(BUZZER_PIN, LOW);  
+    delay(500);
+    digitalWrite(BUZZER_PIN, HIGH);
+}
+
+void joaca_cu_bucle() 
 {
     lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("START JOC!");
+    lcd.print("Joc Pornit!");
+    unsigned long timpStart = millis();
     
-    lcd.setCursor(0, 1);
-    if (level == 4) 
+    for(int i=0; i<4; i++) 
+      lc.clearDisplay(i);
+
+    for (int modul = 0; modul < 4; modul++) 
     {
-        lcd.print("Mod: Infinit");
+        for (int rand = 7; rand >= 0; rand--) 
+        {
+            int blocPozitie = 0;
+            int blocDirectie = 1;
+            int blocLatime = 4; 
+            bool blocPlasat = false; 
+            unsigned long timpMiscare = 0;
+
+            while (blocPlasat == false) 
+            {   
+                if (millis() - timpMiscare > viteza) 
+                {
+                    timpMiscare = millis();
+                    for(int i=0; i<blocLatime; i++) 
+                    {
+                         if(blocPozitie + i < 8) 
+                            lc.setLed(modul, blocPozitie + i, rand, false);
+                    }
+
+                    blocPozitie += blocDirectie;
+                    if (blocPozitie >= (8 - blocLatime) || blocPozitie <= 0) 
+                    {
+                        blocDirectie *= -1;
+                    }
+
+                    for(int i=0; i<blocLatime; i++) 
+                    {
+                        if(blocPozitie + i < 8)
+                            lc.setLed(modul, blocPozitie + i, rand, true);
+                    }
+                }
+                
+                int stareBtn = digitalRead(BTN_START);
+                if (stareBtn == LOW) // trecem mai sus
+                {
+                    blocPlasat = true; 
+                    sunetPlasare(); 
+                    delay(300); 
+                }
+            } 
+        } 
     } 
-    else 
-    {
-        lcd.print("Nivel: ");
-        lcd.print(level);
-    }
+
+    unsigned long timpFinal = millis();
+    float secundeJucate = (timpFinal - timpStart) / 1000.0;
+
+    lcd.clear();
+    lcd.print("CASTIGATOR!");
+    lcd.setCursor(0, 1);
+    lcd.print("Timp: ");
+    lcd.print(secundeJucate);
+    lcd.print("s");
     
-    delay(2000); 
-    select_level(); 
+    sunetCastig(); 
+    
+    delay(4000);
+    jocPornit = false; 
+    select_level();
 }
 
 void setup() 
 {
     lcd.init();        
     lcd.backlight();    
+    for(int i=0; i<4; i++) 
+    {
+        lc.shutdown(i, false); 
+        lc.setIntensity(i, 8); 
+        lc.clearDisplay(i); 
+    }
+    
     pinMode(BTN_SCHIMBA, INPUT_PULLUP); 
     pinMode(BTN_START, INPUT_PULLUP);
-    select_level();     
+    pinMode(BUZZER_PIN, OUTPUT);
+    digitalWrite(BUZZER_PIN, HIGH); //initializam high, canta pe low
+    select_level();      
 }
 
 void loop() 
 {
-    int stareSchimba = digitalRead(BTN_SCHIMBA);
-    int stareStart = digitalRead(BTN_START);
-
-    if (stareSchimba == LOW && lastStateSchimba == HIGH) 
+    if (jocPornit == false) 
     {
-        level++;
-        if (level > totalNivele) 
+        int stareSchimba = digitalRead(BTN_SCHIMBA);
+        int stareStart = digitalRead(BTN_START);
+
+        if (stareSchimba == LOW && lastStateSchimba == HIGH) 
         {
-            level = 1;
+            level++;
+            if (level > totalNivele) level = 1;
+            select_level(); 
+            delay(200);
         }
-        select_level(); 
-        delay(200);
-    }
-    lastStateSchimba = stareSchimba;
+        lastStateSchimba = stareSchimba;
 
-    if (stareStart == LOW && lastStateStart == HIGH) 
+        if (stareStart == LOW && lastStateStart == HIGH) 
+        {
+            jocPornit = true;
+        }
+        lastStateStart = stareStart;
+    } 
+    else 
     {
-        porneste_jocul();
+        joaca_cu_bucle();
     }
-    lastStateStart = stareStart;
 }
